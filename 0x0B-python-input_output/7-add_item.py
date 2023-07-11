4a491d41f2f2594e301af22cf9ec1b7c8e82bb6f
masdari96 <aminemasdari@gmail.com>
2023-07-11 08:35:56 -0700

    last commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """Module that loads, adds and saves arguments to a Python list"""
 from sys import argv
 
-
 load_file = __import__('6-load_from_json_file').load_from_json_file
 save_file = __import__('5-save_to_json_file').save_to_json_file
 
