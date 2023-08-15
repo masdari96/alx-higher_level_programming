1a6b5e9f38727b68f36d6c1cd041c8a305a14ca8
masdari96 <aminemasdari@gmail.com>
2023-08-15 14:39:36 -0700

    task 11 commit

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists all records with a score >= 10
+-- Execute cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score,name FROM second_table where score >= 10 ORDER by socre DESC;
