84bf3ffcae9a9fe50f1f8023fa5d592d88660857
masdari96 <aminemasdari@gmail.com>
2023-08-15 12:54:02 -0700

    correction task 6 commit

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 --  script that prints the full description of the table first_table from the database hbtn_0c_0
---  Exceute: 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+--  Exceute: cat 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 show create table first_table ;
