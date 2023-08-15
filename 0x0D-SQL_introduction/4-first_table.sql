817eb39526c6c42465f256fe45ac5e3e2af32f64
masdari96 <aminemasdari@gmail.com>
2023-08-15 12:44:52 -0700

    task 4 commit

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- script that creates a table called first_table
+-- Exceute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+create table if not exists first_table (id int,name VARCHAR(256));
