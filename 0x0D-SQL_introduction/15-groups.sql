3c58e62f42c563c1e7d769deaa95715b3ea5802c
masdari96 <aminemasdari@gmail.com>
2023-08-15 15:10:07 -0700

    task 15 commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- Script that lists the number of records with the same score in the table second_table
+-- Execute: cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
