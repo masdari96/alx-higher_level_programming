f00482a5128e3afc729d3194f6a72f72caccb470
masdari96 <aminemasdari@gmail.com>
2023-08-15 15:04:53 -0700

    task 13 commit

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script that computes the score average of all records in the table second_table
+-- Execute: cat 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT AVG(score) as average from second_table;
