4d41915f716fadb0126fb17c3d11423ba5b541c5
masdari96 <aminemasdari@gmail.com>
2023-08-15 14:33:32 -0700

    task 10 commit

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table
 -- Execute: cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT * FROM second_table;
+SELECT score, name FROM second_table ORDER BY score DESC;
