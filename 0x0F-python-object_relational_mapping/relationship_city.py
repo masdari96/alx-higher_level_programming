f61e3dbaa389ba0f1d845ba7d710bdef544fd94e
masdari96 <aminemasdari@.com>
2023-09-17 13:23:36 -0700

    task 100 advanced commit

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text, ForeignKey
-from relationship_state import Base
-"""
-    Module that performs creates a States class based off of Base.
-"""
+"""Defines a class City"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
 
 
 class City(Base):
-    """
-        The ``City`` class which inherits from ``Base`` class.
-    """
-    __tablename__ = 'cities'
+    """Module of a city"""
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
